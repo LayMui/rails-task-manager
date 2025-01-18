a8e289e06f89b51af81a60e4b041533cceb0f7e3
LayMui <mui_ume@hotmail.com>
2025-01-18 17:03:07 +0800

    form css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,25 @@
 class TasksController < ApplicationController
-  def task
+  def index
     @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def task_details
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details)
 end
