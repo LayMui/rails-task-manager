7b03664d945efe2d7b56e4d65efab683c581114e
LayMui <mui_ume@hotmail.com>
2025-01-19 10:01:42 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # get "tasks/new", to: "tasks#new", as: :new_task
   # post "tasks", to: "tasks#create"
 
-  # get "tasks/:id", to: "tasks#show", as: :show_task
+  get "tasks/:id", to: "tasks#show", as: :show_task
   # # Update
   # get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   # patch  "tasks/:id",      to: "tasks#update", as: :update_task
